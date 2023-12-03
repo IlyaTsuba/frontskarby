82a4cd319719512e65da2d892371b01979cb4112
MOLOFEEVilya <m_molo@mail.ru>
2023-12-03 16:45:14 +0300

    fixed

--- src/components/SignUp/RegistrationConfirmation/RegistrationConfirmation.js
+++ src/components/SignUp/RegistrationConfirmation/RegistrationConfirmation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
+import { useSelector } from "react-redux"
 
 const RegistrationConfirmation = () => {
   const userInfo = useSelector(state => state.user.user)
